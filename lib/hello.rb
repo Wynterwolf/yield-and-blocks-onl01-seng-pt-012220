24dee736f140777fbd5441d69febfc7ba2af70bb
Samantha Jordan <shinyguns@gmail.com>
2020-01-27 20:39:44 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,12 +4,15 @@ def hello_t(array) #define method
 #using while loop
 
 i = 0 #start counter at 0
+  if block_given? #Check for given block before running
   while i < array.length #loop runs as long as there is an array.legnth
     yield array[i] #grab each element of array while we loop
     i = i + 1 #add one to the counter
   end
   
   array #this returns original array
+else
+  puts "Hey! No block was given!" #graceful fail
 end
 
 # call your method here!
