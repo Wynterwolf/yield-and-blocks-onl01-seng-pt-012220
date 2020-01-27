d7d66d79c77d17c7f803d7c218649dd0180510b8
Samantha Jordan <shinyguns@gmail.com>
2020-01-27 20:28:20 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,10 @@
-def hello_t
+def hello_t(array) #define method
+
+#yeield each element of the array to a block that we will call this method validates_with
+#using while loop
+
+i = 0 #start at 0
+  whil
 
 end
 
